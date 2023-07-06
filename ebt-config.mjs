2f5233b0de4e69a512a03bc0ee5e7a075e8affd3
firepick1 <karl@firepick.org>
2023-07-06 02:43:52 -0700

    Initial commit

---
+++ ebt-config.mjs
@@ -0,0 +1,14 @@
+const EbtConfig = {
+  "appName": "en.sc-voice.net",
+  "basePath": "/ebt-site3/",
+  "homePath": "#/wiki/welcome",
+  "content": {
+    "index": "toc",
+  },
+  "github": {
+    "account": "ebt-site",
+    "repository": "ebt-site3"
+  }
+}
+
+export default EbtConfig;
